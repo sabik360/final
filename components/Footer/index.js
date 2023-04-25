dd227a7aa52685cbbb62e17067b66fe8f525b324
Drastic009 <92353902+Drastic009@users.noreply.github.com>
2023-04-25 17:46:03 +0530

    18

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -15,8 +15,7 @@ const Footer = ({}) => {
             </h1>
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
-            </h1>
-            <Button type="primary">Schedule a call</Button>
+            </h1>            
             <div className="mt-10">
               <Socials />
             </div>
@@ -24,7 +23,7 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        ❤ 
+         
       </h1>
     </>
   );
