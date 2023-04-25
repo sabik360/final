cefa9f6afd5a58aa4818fe96cf168263dd1cff3b
Drastic009 <92353902+Drastic009@users.noreply.github.com>
2023-04-25 17:15:21 +0530

    13

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -24,7 +24,7 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by Sabik
+        ❤ 
       </h1>
     </>
   );
